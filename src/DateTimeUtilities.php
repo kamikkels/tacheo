3ae2329f2a1fafc660bef24e54dbebba5004d096
Kerry M-R <github@kerrymr.com>
2017-06-05 16:04:19 +0930

    KMR: feature branch and start of docs

---
+++ src/DateTimeUtilities.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace dt4a_challenge;
+
+/**
+ * Class DateTimeUtils
+ *
+ * DateTime utilities class to provide useful time between functions
+ *
+ * @package dt4a_challenge
+ */
+
+class DateTimeUtilities
+{
+    /**
+     *
+     */
+}
---
+++ tests/DateTimeUtilitiesTest.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace dt4a_challenge;
+
+use PHPUnit\Framework\TestCase;
+
+use dt4a_challenge\DateTimeUtilities;
+
